51308bbe6b21da6a6e4e270e7262afbf0fb1a66f
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-08-04 15:19:02 -0400

    feat: add serializers for ingredient and category

--- ingredients/serializers.py
+++ ingredients/serializers.py
@@ -1,3 +1,4 @@
+from graphene import relay
 from graphene_django import DjangoObjectType
 
 from ingredients.models import Category, Ingredient
@@ -13,3 +14,33 @@ class IngredientType(DjangoObjectType):
     class Meta:
         model = Ingredient
         fields = fields = "__all__"  # ("id", "name", "notes", "category")
+
+
+class CategoryNode(DjangoObjectType):
+    class Meta:
+        model = Category
+        filter_fields = ["name", "ingredients"]
+        interfaces = (relay.Node,)
+
+
+class IngredientNode(DjangoObjectType):
+    class Meta:
+        model = Ingredient
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            "name": ["exact", "icontains", "istartswith"],
+            "notes": ["exact", "icontains"],
+            "category": ["exact"],
+            "category__name": ["exact"],
+        }
+        interfaces = (relay.Node,)
+
+
+class CategoryConnection(relay.Connection):
+    class Meta:
+        node = CategoryNode
+
+
+class IngredientConnection(relay.Connection):
+    class Meta:
+        node = IngredientNode
