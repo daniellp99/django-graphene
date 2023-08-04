84d21639a159e3d0e6f20696445e8cf90bc32fa2
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-08-04 12:21:42 -0400

    fix: update schema import

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
-from core import schema
+from core.schema import schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
