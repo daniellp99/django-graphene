56ccae220c56bafd1fafecf60799aa2a025f3470
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-08-04 11:48:07 -0400

    feat: add models to admin site and load fixtures

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from ingredients.models import Category, Ingredient
+
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(Category)
