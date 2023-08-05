9cec223bba03bf8c545c0848a6d5e02b77ba112a
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-08-05 15:31:30 -0400

    feat: update ingredient schema to accept mutation

--- ingredients/schema.py
+++ ingredients/schema.py
@@ -1,8 +1,6 @@
-
 import graphene
 
 from .query import Queries
+from .mutation import Mutations
 
-schema = graphene.Schema(
-    query=Queries,
-)
+schema = graphene.Schema(query=Queries, mutation=Mutations)
